4270faa6b1b78cf907dda269e2a52ae6eccc1bec
Njamen <njamen.n3@gmail.com>
2023-03-09 19:52:52 +0100

    commit saved

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{x} + {y} = {z}".format(x=a, y=b, z=add_0.add(a, b)))
+    print("{x} + {y} = {z}".format(x=a, y=b, z=add(a, b)))
