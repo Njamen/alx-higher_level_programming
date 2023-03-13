8aded21ed3c59915bbe448c087d1f52555f650b0
Njamen <njamen.n3@gmail.com>
2023-03-13 15:09:41 +0100

    commit saved

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if my_list:
-        for x in my_list:
-            print("{n}".format(n=x))
+    for x in range(len(my_list)):
+        print("{n}".format(n=my_list[x]))
