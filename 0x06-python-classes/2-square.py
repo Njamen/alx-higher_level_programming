fa4a5aba6f8a93775b65f64092dbd43cb05399a7
Njamen <njamen.n3@gmail.com>
2023-04-03 14:26:59 +0100

    commit saved

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+""" class Square module"""
+
+
 class Square:
     def __init__(self, size=0):
+        """init square
+        Args:
+            size (int): size of the square
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
