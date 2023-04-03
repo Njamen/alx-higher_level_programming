d44d513537840a6a53f7a7c682b72e327a058df5
Njamen <njamen.n3@gmail.com>
2023-04-03 19:10:50 +0100

    commit saved

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -5,4 +5,3 @@
 class Rectangle:
     """Class Rectangle"""
     pass
-
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Module for rectangle"""
+
+
+class Rectangle:
+    """Class Rectangle"""
+    def width(self):
+        """get width"""
+        return self.__width
+
+    def width(self, value):
+        """set width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def height(self):
+        """get heigth"""
+        return self.__height
+
+    def height(self, value):
+        """set height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __init__(self, width, height):
+        """construtor"""
+        self.__width = width
+        self.__height = height
