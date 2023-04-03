b4da6c2d415f3fc5e559b293d9228043d071ab75
Njamen <njamen.n3@gmail.com>
2023-04-03 13:53:25 +0100

    commit saved

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+""" class Square module"""
+
+
 class Square:
+    """ class square"""
     def __init__(self, size):
-        self.__size = size
+        """init square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size  #: size of the square
