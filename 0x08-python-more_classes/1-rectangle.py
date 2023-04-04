fdf6a41fd49a7fdb905f0097c4a22dc6acc4171a
Njamen <njamen.n3@gmail.com>
2023-04-04 09:52:58 +0100

    commit saved

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,10 +4,12 @@
 
 class Rectangle:
     """Class Rectangle"""
+    @property
     def width(self):
         """get width"""
         return self.__width
 
+    @width.setter
     def width(self, value):
         """set width"""
         if not isinstance(value, int):
@@ -16,10 +18,12 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
     def height(self):
         """get heigth"""
         return self.__height
 
+    @height.setter
     def height(self, value):
         """set height"""
         if not isinstance(value, int):
@@ -28,7 +32,7 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-    def __init__(self, width, height):
+    def __init__(self, width=0, height=0):
         """construtor"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
